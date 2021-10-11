ff2904110191a72f3b01091ab4b584bb8da40358
saurav <sauravsingh25625@gmail.com>
2021-10-11 22:07:07 +0530

    initial commit

---
+++ src/FiconaciiSeries.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FiconaciiSeries {
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        for(int i=0;i<s;i++){
+            System.out.println(i);
+            i= i+i+1;
+
+        }
+
+
+
+
+
+
+
+    }
+}
